5c3ce782626e43931bcb22e7e062a4c604d1847a
Your Name <akhilkumarchandragiri@gmail.com>
2021-12-25 22:45:52 +0530

    day 14

---
+++ DataStructureDemo/DataStructureDemo/LinkedList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureDemo
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        internal void add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked is empty");
+                return;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine(" Invalid Postion");
+            }
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position---- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node Node = new Node(data);
+                        Node.next = this.head.next;
+                        head.next = Node;
+                        break;
+                    }
+                    head = head.next;
+                }
+            }
+            if (position != 1)
+            {
+                Console.WriteLine(" position is out of range ");
+            }
+            Console.WriteLine(" inserted value " + head);
+        }     return head;
+    }
+    internal Node RemoveFirstNode()
+    {
+        if (this.head == null)
+        {
+            return null;
+        }
+        this.head = this.head.next;
+        return this.head;
+    }
+    internal Node RemoveLastNode()
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        if (head.next == null)
+        {
+            return null;
+        }
+        Node NewNode = head;
+        while (NewNode.next.next != null)
+        {
+            NewNode = NewNode.next;
+        }
+        NewNode.next = null;
+        return head;
+    }
+
+}
+
---
+++ DataStructureDemo/DataStructureDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            LinkedList list = new LinkedList();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+        }
+    }
+}
